fa5d5d54461cc8a99f5822cfbd4fdb212848df78
Kiran Arun Kshirsagar <82576116+mrkk7999@users.noreply.github.com>
2022-08-06 00:11:00 +0530

    Update SingleElementInASortedArray.go
    Added approach and question neatly

--- Searching/Binary/SingleElementInASortedArray.go
+++ Searching/Binary/SingleElementInASortedArray.go
@@ -1,25 +1,45 @@
+// Question
+// You are given a sorted array consisting of only integers where every element appears exactly twice, 
+// except for one element which appears exactly once.
+//Return the single element that appears only once.
+// Your solution must run in O(log n) time and O(1) space.
+
 package main
 
 import "fmt"
 
+// Approach
 func singleNonDuplicate(nums []int) (int, int) {
 	N := len(nums)
 	left, right := 0, N-1
+	// loop
 	for left <= right {
 		M := left + (right-left)/2
+		// checks if single element present at the edges 
 		if (M == 0 || nums[M] != nums[M-1]) && (M == N-1 || nums[M] != nums[M+1]) {
 			return nums[M], M
 		}
+		// it implies element at M & M+1 position are same
 		if M == 0 || nums[M] != nums[M-1] {
+			// that means our mid is at first position since in above condition we found mid and mid next are same and 
+			// if mid is even that means no single inserted till now
+			// so we can go on right
 			if M%2 == 0 {
 				left = M + 1
-			} else {
+			} else { 
+				// else we're going right
 				right = M - 1
 			}
-		} else {
+		} else { 
+			// this means element at M & M's previous are same
 			if M%2 == 0 {
+				// if second element is even that means single element occured earlier
+				// so go left
+				// that means lower right
 				right = M - 1
 			} else {
+				// go right
+				// means increase left
 				left = M + 1
 			}
 		}
