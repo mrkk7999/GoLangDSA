bddadf8eee754698924c7f97dcd10b69a9a3d831
mrkk7999 <82576116+mrkk7999@users.noreply.github.com>
2022-08-05 17:54:14 +0530

    Added Binary Search

--- Searching/binarySearchRecursive.go
+++ Searching/binarySearchRecursive.go
@@ -2,15 +2,15 @@ package main
 
 import "fmt"
 
-func binSearchItr(arr []int, target int, left, right int) (int, bool) {
+func binSearchRec(arr []int, target int, left, right int) (int, bool) {
 	for left <= right {
 		mid := left + (right-left)/2
 		if target == arr[mid] {
 			return mid, true
 		} else if target < arr[mid] {
-			right = mid - 1
+			binSearchRec(arr, target, left, mid-1)
 		} else if target > arr[mid] {
-			left = mid + 1
+			binSearchRec(arr, target, mid+1, right)
 		}
 	}
 	return -1, false
@@ -18,7 +18,7 @@ func binSearchItr(arr []int, target int, left, right int) (int, bool) {
 
 func main() {
 	var arr = []int{12, 43, 65, 23}
-	Index, Result := binSearchItr(arr, 43, 0, len(arr)-1)
+	Index, Result := binSearchRec(arr, 43, 0, len(arr)-1)
 	if Result || Index != -1 {
 		fmt.Printf("Element found at index :%d", Index)
 	} else {
