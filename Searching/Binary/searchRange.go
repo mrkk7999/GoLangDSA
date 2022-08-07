4cb159fef6da0498738eeb8aacac18a9c2e8cd98
mrkk7999 <82576116+mrkk7999@users.noreply.github.com>
2022-08-07 11:58:56 +0530

    Create searchRange.go

---
+++ Searching/Binary/searchRange.go
@@ -0,0 +1,23 @@
+package main
+
+// Question
+// Given a sorted array of integers A(0 based index) of size N,
+// find the starting and the ending position of a given integer B in array A.
+
+// Your algorithm's runtime complexity must be in the order of O(log n).
+
+// Return an array of size 2, such that the first element = starting position of B in A and
+// the second element = ending position of B in A, if B is not found in A return [-1, -1].
+
+// Approach
+// Bruteforce
+//
+
+// Optimal
+//
+func searchRange(A []int, B int) []int {
+	return nil
+}
+func main() {
+
+}
