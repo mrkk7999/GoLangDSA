f61528a7838a095f5f86adba3e811f5847742df8
Kiran Arun Kshirsagar <82576116+mrkk7999@users.noreply.github.com>
2022-08-09 21:34:30 +0530

    Update MaximumHeightOfStaircase.go

--- Searching/Questions/MaximumHeightOfStaircase.go
+++ Searching/Questions/MaximumHeightOfStaircase.go
@@ -1,10 +1,24 @@
+// Questions
+// Given an integer A representing the number of square blocks. The height of each square block is 1. The task is to create a staircase of max-height using these blocks.
+
+// The first stair would require only one block, and the second stair would require two blocks, and so on.
+
+// Find and return the maximum height of the staircase.
+
 package main
 
 import "fmt"
 
+// Problem approach
+// keep two variables sum and count 
+// keep adding to sum and checking if it's not crossing the given integer value
+// if it's then break the loop and come out of the loop and return the count
+// else increase the count counter
+
 func solve(A int) int {
 	var sum, count = 0, 0
 	for i := 1; i <= A; i++ {
+		// make sure you'll add first and then check the breaking condition
 		sum = sum + i
 		if sum > A {
 			break
